1c242e57dc885dabdb32428c69a69da2dd17d634
sdpercy <scottpercy@hotmail.com>
2021-08-17 22:09:38 -0230

    added POST api/thoughts to thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -38,5 +38,23 @@ const thoughtController = {
         });
     },
     //POST /api/thoughts
-    
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(dbThoughtData => {
+            User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: dbThoughtData._id } },
+                { new: true }
+            )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+        })
+        .catch(err => res.status(400).json(err));
+    },
 }
